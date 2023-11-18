a1b2fff3133b0db6e42819e5a1bb8e379d5a8eb1
M. Babenko <babenoff.code@outlook.com>
2023-11-19 01:55:40 +0300

    build(Router): Реализация роутера

--- cmd/urlsh/main.go
+++ cmd/urlsh/main.go
@@ -9,6 +9,7 @@ import (
 	"syscall"
 
 	"github.com/babenow/url_shortener/intrernal/config"
+	"github.com/babenow/url_shortener/intrernal/http-server/middleware/logger"
 	"github.com/babenow/url_shortener/intrernal/lib/logger/sl"
 	"github.com/babenow/url_shortener/intrernal/storage/sqlite"
 	"github.com/go-chi/chi/v5"
@@ -21,27 +22,29 @@ func main() {
 
 	cfg := config.Instance()
 
-	logger := setupLogger(cfg.Env)
-	logger.Info("starting application", slog.String("env", cfg.Env))
-	logger.Debug("debug messages are enabled")
+	log := setuplog(cfg.Env)
+	log.Info("starting application", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 
 	storage, err := sqlite.New(ctx)
 	if err != nil {
-		logger.Error("can not initialize storage", sl.Err(err))
+		log.Error("can not initialize storage", sl.Err(err))
 		os.Exit(1)
 	}
 	_ = storage
 
-	// TODO: init router
 	router := chi.NewRouter()
 
 	// middleware
 	router.Use(middleware.RequestID, middleware.RealIP)
-	router.Use(middleware.Logger)
+	router.Use(logger.New(log))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat) // TODO: привязка к роутеру chi
+
 	// TODO: run server
 }
 
-func setupLogger(env string) *slog.Logger {
+func setuplog(env string) *slog.Logger {
 	var logger *slog.Logger
 	switch env {
 	case config.EnvLocal:
